7e26dbee8c849d5fdac59f4a63ee549031a85dc4
Bastian Krause <bst@pengutronix.de>
2026-01-08 13:26:00 +0100

    src/config_file: pass complete RaucConfig to parse_slots()
    Most other parser functions get the reference to the RaucConfig passed,
    but not parse_slots(). Until now, parse_slots() only use the
    RaucConfig's data_directory.
    
    A future commit will need to check for RaucConfig's system_bootloader,
    so pass a reference to the RaucConfig instead of passing all options
    separately as preparation for that.
    
    Signed-off-by: Bastian Krause <bst@pengutronix.de>
    (cherry picked from commit 64e43e0e59ca59b6b5ae69b09fa94264a3c8fdec)

--- src/config_file.c
+++ src/config_file.c
@@ -338,7 +338,7 @@ static gboolean r_event_log_parse_config_sections(GKeyFile *key_file, RaucConfig
 	return TRUE;
 }
 
-static GHashTable *parse_slots(const char *filename, const char *data_directory, GKeyFile *key_file, GError **error)
+static GHashTable *parse_slots(const char *filename, RaucConfig *c, GKeyFile *key_file, GError **error)
 {
 	GError *ierror = NULL;
 	g_auto(GStrv) groups = NULL;
@@ -375,8 +375,8 @@ static GHashTable *parse_slots(const char *filename, const char *data_directory,
 
 			/* If we have a data_directory, use a slot.<class>.<index>
 			 * subdirectory for per-slot data. */
-			if (data_directory)
-				slot->data_directory = g_build_filename(data_directory, groups[i], NULL);
+			if (c->data_directory)
+				slot->data_directory = g_build_filename(c->data_directory, groups[i], NULL);
 
 			slot->description = key_file_consume_string(key_file, groups[i], "description", NULL);
 
@@ -1249,7 +1249,7 @@ gboolean load_config(const gchar *filename, RaucConfig **config, GError **error)
 	}
 
 	/* parse [slot.*.#] sections */
-	c->slots = parse_slots(filename, c->data_directory, key_file, &ierror);
+	c->slots = parse_slots(filename, c, key_file, &ierror);
 	if (!c->slots) {
 		g_propagate_error(error, ierror);
 		return FALSE;
